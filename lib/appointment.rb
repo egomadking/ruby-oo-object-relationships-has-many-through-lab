dcedcd465ff9e10de4cdf3c4b0fb06835bed37bc
Roy Mosby <ego.madking@gmail.com>
2020-12-22 19:23:28 -0500

    commit tree 5870d7af2865f48a65f00254309ba368dec05879

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,19 @@
 
+class Appointment
+
+  attr_accessor :date, :doctor, :patient
+
+  @@all = []
+
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
